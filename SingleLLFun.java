4557581095891a02de1aef45620dce3825e48b45
Kiran Gangoor <kirankumargangoor27@gmail.com>
2024-06-15 12:25:03 +0530

    SingleLL is added

---
+++ SingleLLFun.java
@@ -0,0 +1,10 @@
+public class SingleLLFun {
+    public static void main(String[] args) {
+        SingleLinkedList list= new SingleLinkedList();
+        list.insertFirst(78);
+        list.insertFirst(7);
+        list.insertFirst(8);
+        list.insertFirst(90);
+        list.display();
+    }
+}
